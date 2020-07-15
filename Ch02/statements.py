7810fd76c1ce4b5656c84691b42a6bbb3f1a4159
Tommy Lau <tommy.lhg@gmail.com>
2020-07-15 08:30:38 +0800

    02.04 - Expressions and statements

---
+++ Ch02/statements.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2020 Tommy http://tommy.net.cn/
+
+import platform
+
+version = platform.python_version()
+
+print('The python version is {}'.format(version))
