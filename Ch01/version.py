8ba9e972cdf99e7b3ed60eaebf80290c179c4f35
Tommy Lau <tommy.lhg@gmail.com>
2020-07-15 08:25:17 +0800

    01.01 - Installing Python and PyCharm

---
+++ Ch01/version.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2020 Tommy http://tommy.net.cn/
+
+import platform
+
+print('The python version is {}'.format(platform.python_version()))
